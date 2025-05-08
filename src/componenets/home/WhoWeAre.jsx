7158f13bb138c541cb39394a1514019d3f04d22f
famim2002 <famimhayatyash@gmail.com>
2025-05-08 10:43:56 +0600

    contetnt_add

---
+++ src/componenets/home/WhoWeAre.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const WhoWeAre = () => {
+  return (
+    <section className="px-3 py-20">
+      <div className="container">
+        <div
+          className="flex flex-col  justify-between lg:gap-7
+        lg:flex-row"
+        >
+          <div className="flex flex-col gap-10">
+            <h2
+              className=" text-4xl font-bold font-headerFont max-w-4xl tracking-tighter
+            md:text-7xl"
+            >
+              <span
+                className="inline-block font-tagFont text-2xl font-medium w-fit pr-10 pb-5 tracking-tight relative after
+              after:absolute  after:w-40  after:left-0 after:h-1 after:top-8 after:bg-brandColor active:bg-lightBrandColor"
+              >
+                01 who we are
+              </span>
+              HAVE A BRILLIANT IDEA BOOST THE
+              <span className=" inline-block text-sceondryColor">
+                GROWTH DEVELOPMENT
+              </span>
+              AGENCY YOUR BRANDING !
+            </h2>
+            <div className="flex flex-col ">
+              <div className="flex flex-col gap-2">
+                <div className="flex gap-2">
+                  <img
+                    className="w-15 rounded-[100px] border-3 border-lightBrandColor "
+                    src="/client (1).jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-15 rounded-[100px] border-3 border-lightBrandColor "
+                    src="/client (2).jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-15 rounded-[100px] border-3 border-lightBrandColor "
+                    src="/client (3).jpg"
+                    alt=""
+                  />
+                </div>
+                <small className="text-xl text-semibold text-sceondryColor ">
+                  <span className="text-xl text-prmiaryColor font-semibold">
+                    32+
+                  </span>{" "}
+                  happy clients
+                </small>
+              </div>
+            </div>
+          </div>
+          <div
+            className="relative max-w-sm  m-auto "
+          >
+            <img className=" rotate-90
+          lg:rounded-bl-[40%] lg:rotate-0" src="/idea.jpg" alt="" />
+            <div className="absolute px-10 mx-auto py-15 bg-stone-100 translate-y-[-250px] 
+            sm:translate-x-[90px]
+              lg:rounded-bl-[40%]">
+              <h3 className="font-semibold text-center text-sceondryColor max-w-50">
+                Make your business prosper with our great team of experts. We’ll
+                make your.
+              </h3>
+              <p className="py-6 text-9xl  text-center text-prmiaryColor font-headerFont font-semibold tracking-tighter">1.8x</p>
+              <p className="font-semibold  text-center text-prmiaryColor">FASTER SERVICE</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhoWeAre;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../componenets/home/Banner'
+import WhoWeAre from '../componenets/home/WhoWeAre';
 
 const Home = () => {
   return (
     <>
       <Banner/>
+      <WhoWeAre/>
     </>
   );
 }
