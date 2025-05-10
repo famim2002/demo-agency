4fbadf4768e61c3561e59f6d6864ef34d4f86ff9
famim2002 <famimhayatyash@gmail.com>
2025-05-10 16:06:43 +0600

    content-add

---
+++ src/componenets/home/ParallaxEffect.jsx
@@ -0,0 +1,9 @@
+import { Parallax } from "react-parallax";
+
+const ParallaxEffect = () => (
+  <Parallax bgImage="/parallax-back.jpg" bgImageAlt="the cat" strength={800} className="h-dvh">
+  
+  </Parallax>
+);
+
+export default ParallaxEffect;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import Banner from '../componenets/home/Banner'
-import WhoWeAre from '../componenets/home/WhoWeAre';
-import Charge from '../componenets/home/Charge'
-import Technologies from '../componenets/home/Technologies'
-import Services from '../componenets/home/Services'
-import Testimonial from '../componenets/home/Testimonial'
-import Team from '../componenets/home/Team'
-import Question from '../componenets/home/Question'
-import ParallaxBack from '../componenets/home/ParallaxBack'
+import React from "react";
+import Banner from "../componenets/home/Banner";
+import WhoWeAre from "../componenets/home/WhoWeAre";
+import Charge from "../componenets/home/Charge";
+import Technologies from "../componenets/home/Technologies";
+import Services from "../componenets/home/Services";
+import Testimonial from "../componenets/home/Testimonial";
+import Team from "../componenets/home/Team";
+import Question from "../componenets/home/Question";
+import ParallaxEffect from "../componenets/home/ParallaxEffect";
 
 const Home = () => {
   return (
     <>
-      <Banner/>
+      <Banner />
       <WhoWeAre />
       <Charge />
       <Technologies />
       <Services />
-      <ParallaxBack/>
+      <ParallaxEffect />
       <Testimonial />
       <Team />
-      <Question/>
+      <Question />
     </>
   );
-}
+};
 
-export default Home
+export default Home;
